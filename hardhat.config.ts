63de595b5aa3e81241dc88d225051af3d11e1d16
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2023-02-17 16:29:51 +0100

    feat: verify  contract on Polyscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-etherscan";
 
 import dotenv  from "dotenv";
 dotenv.config()
@@ -11,9 +12,12 @@ dotenv.config()
 const ALFAJORES_URL = process.env.ALFAJORES_URL || "";
 const POLYGON_URL = process.env.POLYGON_URL || "";
 const SHARDEUM_URL = process.env.SHARDEUM_URL || "";
+const MUMBAI_URL = process.env.MUMBAI_URL || "";
 const ALFAJORES_MNEMONIC = process.env.ALFAJORES_MNEMONIC || "";
 const POLYGON_MNEMONIC = process.env.POLYGON_MNEMONIC || "";
 const SHARDEUM_MNEMONIC = process.env.SHARDEUM_MNEMONIC || "";
+const MUMBAI_MNEMONIC = process.env.MUMBAI_MNEMONIC || "";
+const POLYSCAN_APIKEY = process.env.POLYSCAN_APIKEY || "";
 const GAS_PRICE = 2_000_000_000;
 
 
@@ -31,11 +35,21 @@ const config: HardhatUserConfig = {
       accounts : [POLYGON_MNEMONIC]
     },
 
+    Mumbai : {
+      url: MUMBAI_URL,
+      gasPrice : GAS_PRICE,
+      accounts : [MUMBAI_MNEMONIC]
+    },
+
     Shardeum : {
       url: SHARDEUM_URL,
       gasPrice : GAS_PRICE,
       accounts : [SHARDEUM_MNEMONIC]
     },
+  },
+
+  etherscan : {
+    apiKey: POLYSCAN_APIKEY
   },
 
   solidity: {
